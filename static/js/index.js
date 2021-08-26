95c930ccc4de09eb3282bb3e5e5fcd84dc4e90ba
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-08-26 22:02:24 +0100

    darkmode style II

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,4 @@
 let switchBtn = document.querySelector(".switch-btn");
-// let switchBox = document.querySelector(".switch-box");
 let container = document.querySelector(".container");
 let sun = document.querySelector(".icon-tabler-sun");
 let moon = document.querySelector(".icon-tabler-moon");
@@ -7,12 +6,14 @@ let glass = document.querySelector(".card__glass");
 let heading = document.querySelector(".heading");
 let text = document.querySelector(".text");
 let label = document.querySelector(".card__label");
+let label2 = document.querySelector(".card__label--psd");
 let svg = document.querySelector(".icon-tabler-mail");
 let svg2 = document.querySelector(".icon-tabler-key");
 let input = document.querySelector("#username");
 let input2 = document.querySelector("#password");
-console.log(input);
-console.log(input2);
+let rememberMe = document.querySelector(".card__label--checkbox");
+let textSub = document.querySelector(".text--sub");
+let checkbox = document.querySelector("#remember");
 
 const switchAll = () => {
   switchBtn.classList.toggle("right");
@@ -24,10 +25,14 @@ const switchAll = () => {
   heading.classList.toggle("dark-heading");
   text.classList.toggle("dark-text");
   label.classList.toggle("dark-label");
+  label2.classList.toggle("dark-label");
   svg.classList.toggle("dark-input-svg");
   svg2.classList.toggle("dark-input-svg");
   input.classList.toggle("dark-input");
   input2.classList.toggle("dark-input");
+  rememberMe.classList.toggle("dark-remember");
+  textSub.classList.toggle("dark-sub");
+  checkbox.classList.toggle("remember__checkbox--dark");
 };
 
 switchBtn.addEventListener("click", switchAll);
